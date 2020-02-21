9b3a7ca78a9267de3f87232dfc23af5f385364a0
joscherrer <jonathan.s.scherrer@gmail.com>
2020-02-21 14:01:48 +0100

    fix python lint

--- filter_plugins/custom.py
+++ filter_plugins/custom.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 import re
 
+
 def regex_matches(_key, _regex):
     for r in _regex:
         m = re.compile("(?:" + r + r")\Z")
         if m.match(_key):
             return True
     return False
+
 
 def dict_process(_dict, _regex, _strip=False):
     """This function processes dicts with a regex
@@ -19,15 +21,18 @@ def dict_process(_dict, _regex, _strip=False):
     for key, value in _dict.items():
         if regex_matches(key, _regex) is not _strip:
             tmp_dict[key] = value
-    # Return tmp_dict if _regex list 
+    # Return tmp_dict if _regex list
     # isn't empty, otherwise return _dict
     return tmp_dict if _regex else _dict
+
 
 def dict_filter_keys(_dict, _regex):
     return dict_process(_dict, _regex, False)
 
+
 def dict_strip_keys(_dict, _regex):
     return dict_process(_dict, _regex, True)
+
 
 class FilterModule(object):
     def filters(self):
